ff2a04df9eedf97a9cfc71491ada1c6ca173a387
JaneBdemented <jacqueline.waitman@gmail.com>
2015-03-26 17:38:26 -0400

    updated for indexing errors

--- HN2SDN_GUI/store.php
+++ HN2SDN_GUI/store.php
@@ -25,51 +25,53 @@ $st = Array();
 $endt = Array();
 $speed = Array();
 $date = new DateTime();
+$m = 0;
 for ($i = 0; $i<count($value);$i++){
-	$usr[$i] = $value[$i];
+	$usr[$m] = $value[$i];
 	if($value[$i] =='Denied'){
-		$block[$i] = 1;
+		$block[$m] = 1;
 	}else{
-		$block[$i] = 0;
+		$block[$m] = 0;
 	}
 	if($value[$i+2]=='No Limit'){
-		$DL[$i] = $Data;
+		$DL[$m] = $Data;
 	}else if($value[$i+3]=='MB'){
-		$DL[$i] = ($value[$i+2])*1000000;
+		$DL[$m] = ($value[$i+2])*1000000;
 	}else{
-		$DL[$i] = ($value[$i+2])*1000000000;
+		$DL[$m] = ($value[$i+2])*1000000000;
 	}
 	if($value[$i+4]=='No Limit'){
-		$st[$i] = "00:00:00";
+		$st[$m] = "00:00:00";
 	}else if($value[$i+5]='AM'){
 		$t1 = explode(':',$value[$i+4]);
-		$st[$i] = $value[$i+4];
+		$st[$m] = $value[$i+4];
 	}else{
 		$t1 = explode(':',$value[$i+4]);
 		$h = ($t1)+12;
-		$st[$i] = " ".$h.":".$t1[1].":".$t1[2]." "; //this will not work but for now
+		$st[$m] = " ".$h.":".$t1[1].":".$t1[2]." "; //this will not work but for now
 	}
 	if($value[$i+6]=='No Limit'){
 		$endt[$i] = "00:00:00";
 	}else if($value[$i+7]=='AM'){
 		$t2 = explode(':',$value[$i+6]);
-		$endt[$i] = $value[$i+6];
+		$endt[$m] = $value[$i+6];
 	}else{
 		$t2 = explode(':',$value[$i+6]);
 		$h2 = ($t2[0])+12;
-		$endt[$i] = " ".$h2.":".$t2[1].":".$t2[2]." "; //this will not work but for now
+		$endt[$m] = " ".$h2.":".$t2[1].":".$t2[2]." "; //this will not work but for now
 	}
 	switch($value[$i+8]){
-		case('No Limit'):	$speed[$i] = $BW;
+		case('No Limit'):	$speed[$m] = $BW;
 							break;
-		case('Fast'):		$speed[$i] = ($BW)*0.5;
+		case('Fast'):		$speed[$m] = ($BW)*0.5;
 							break;
-		case('Medium'):		$speed[$i] = ($BW)*0.25;
+		case('Medium'):		$speed[$m] = ($BW)*0.25;
 							break;				
-		case('Slow'):		$speed[$i] = ($BW)*0.1;
+		case('Slow'):		$speed[$m] = ($BW)*0.1;
 							break;				
 	}
 	$i=$i+8;
+	$m=$m+1;
 }
 
 
